9a31582f77b20539691298df4125263eb87e99a2
haribhatt34 <haribhatt34@gmail.com>
2019-08-10 19:27:25 +0530

    Update SimulatorApplication.java

--- src/main/java/com/example/demo/SimulatorApplication.java
+++ src/main/java/com/example/demo/SimulatorApplication.java
@@ -3,18 +3,17 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SimulatorApplication extends SpringBootServletInitializer {
+public class PaymentProcessorApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimulatorApplication.class, args);
+		SpringApplication.run(PaymentProcessorApplication.class, args);
 	}
 	
-	@Override
+    @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(SimulatorApplication.class);
+        return builder.sources(PaymentProcessorApplication.class);
     }
-
 }
