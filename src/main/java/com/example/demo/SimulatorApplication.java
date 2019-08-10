31a5fe0e377812529715843e09ca7f2a338ad320
haribhatt34 <haribhatt34@gmail.com>
2019-08-10 19:18:18 +0530

    extended main application class
    for tomcat spring boot support

--- src/main/java/com/example/demo/SimulatorApplication.java
+++ src/main/java/com/example/demo/SimulatorApplication.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-public class SimulatorApplication {
+public class SimulatorApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimulatorApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SimulatorApplication.class);
+    }
 
 }
