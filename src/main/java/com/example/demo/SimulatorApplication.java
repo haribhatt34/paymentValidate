aac838e872ff2f8e670152205435a6aae6dd02cb
haribhatt34 <haribhatt34@gmail.com>
2019-08-10 19:29:47 +0530

    rolled back to earlier

--- src/main/java/com/example/demo/SimulatorApplication.java
+++ src/main/java/com/example/demo/SimulatorApplication.java
@@ -2,18 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PaymentProcessorApplication extends SpringBootServletInitializer {
+public class SimulatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaymentProcessorApplication.class, args);
+		SpringApplication.run(SimulatorApplication.class, args);
 	}
-	
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(PaymentProcessorApplication.class);
-    }
+
 }
